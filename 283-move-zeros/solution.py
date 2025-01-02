98f678d18982eca20d5d15174ca05f27f66819f0
Quang Do <quang.do@colorado.edu>
2025-01-01 19:43:11 -0700

    feat: add 283

--- 283-move-zeros/solution.py
+++ 283-move-zeros/solution.py
@@ -1,20 +1,19 @@
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
-        size = len(nums)
         head_zero_pointer = -1
 
-        for i in range(size):
+        for i in range(len(nums)):
             if nums[i] == 0:
                 if head_zero_pointer == -1:
                     head_zero_pointer = i
-            elif head_zero_pointer != -1 and i > head_zero_pointer:
+            elif head_zero_pointer != -1:
                 nums[head_zero_pointer] = nums[i]
                 nums[i] = 0
                 head_zero_pointer += 1
 
 
 sol = Solution()
-my_list = [0, 0, 1]
+my_list = [0,1,0,3,12]
 sol.moveZeroes(my_list)
 
 print(my_list)
