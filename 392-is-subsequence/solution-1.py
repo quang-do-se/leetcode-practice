8ecc6e41fbb5d2867543b46d2c9f7daa978b317a
Quang Do <quang.do@colorado.edu>
2025-01-03 00:51:59 -0700

    feat: update 392

---
+++ 392-is-subsequence/solution-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        slow = fast = 0
+
+        while slow < len(s) and fast < len(t):
+            if s[slow] == t[fast]:
+                slow += 1
+            fast += 1
+        
+        return slow == len(s)
+
+    
+sol = Solution()
+print(sol.isSubsequence("b", "abc"))
--- 392-is-subsequence/solution.py
+++ 392-is-subsequence/solution.py
@@ -4,16 +4,15 @@ class Solution:
             return True
 
         slow = 0
-        count_match = 0
 
         for c in t:
             if c == s[slow]:
                 slow += 1
-                count_match +=1
-                if count_match == len(s):
+                if slow == len(s):
                     return True
         
         return False
+
     
 sol = Solution()
 print(sol.isSubsequence("b", "abc"))
