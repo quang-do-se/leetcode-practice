bc79f0cffc6ac1151d58d1a475d598296a432e71
Quang Do <quang.do@colorado.edu>
2025-01-11 17:15:57 -0700

    feat: add 345

--- 345-reverse-vowels-of-a-string/solution-1.py
+++ 345-reverse-vowels-of-a-string/solution-1.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = ['a', 'e', 'i', 'o', 'u']
-        found_vowels = []
 
         l = 0
         r = len(s) - 1
@@ -12,10 +11,9 @@ class Solution:
                 while a[r].lower() not in vowels:
                     r -= 1
 
-                # swap   
-                if r > l:
-                    a[l], a[r] = a[r], a[l]
-                    r -= 1
+                # swap
+                a[l], a[r] = a[r], a[l]
+                r -= 1
 
             l += 1
 
@@ -23,7 +21,7 @@ class Solution:
 
 
 sol = Solution()
-print(sol.reverseVowels("All"))
+print(sol.reverseVowels("All") == "All")
 print(sol.reverseVowels("IceCreAm") == "AceCreIm")
 print(sol.reverseVowels("leetcode") == "leotcede")
 print(sol.reverseVowels("") == "")
