fc862c5e33b3f5cd8547912d610a843996044c01
Quang Do <quang.do@colorado.edu>
2025-01-12 19:43:10 -0700

    feat: update 151

---
+++ 151-reverse-words-in-a-string/solution-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(s.split()[::-1])
+
+
+sol = Solution()
+print(sol.reverseWords("the sky is blue") == "blue is sky the")
+print(sol.reverseWords("   hello   world ") == "world hello")
+print(sol.reverseWords("a good   example") == "example good a")
+print(sol.reverseWords("a good   example 123 456") == "456 123 example good a")
