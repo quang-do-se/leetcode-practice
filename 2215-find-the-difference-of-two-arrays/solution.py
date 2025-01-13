c53bcddf6f78e959fd131e8ef3d6e56e583d2b08
Quang Do <quang.do@colorado.edu>
2025-01-12 20:33:26 -0700

    feat: add 2215

---
+++ 2215-find-the-difference-of-two-arrays/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
+        dict1 = {}
+        dict2 = {}
+        for n in nums1:
+            dict1[n] = None
+
+        for n in nums2:
+            dict2[n] = None
+
+        diff1, diff2 = [], []
+        for key in dict1:
+            if key not in dict2:
+                diff1.append(key)
+
+        for key in dict2:
+            if key not in dict1:
+                diff2.append(key)
+
+        return [diff1, diff2]
+
+
+sol = Solution()
+print(sol.findDifference([1,2,3] ,[2,4,6]))
+print(sol.findDifference([1,2,3,3] ,[1,1,2,2]))
+        
