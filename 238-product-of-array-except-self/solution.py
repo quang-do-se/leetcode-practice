db1e176e9427fe3836847403b06296178c3d43ae
Quang Do <quang.do@colorado.edu>
2025-01-14 17:09:00 -0700

    feat: update 238

--- 238-product-of-array-except-self/solution.py
+++ 238-product-of-array-except-self/solution.py
@@ -1,22 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        prefix = [nums[0]] * len(nums)
-        suffix = [nums[len(nums) - 1]] * len(nums)
+        n = len(nums)
+        prefix = [0] * n
+        suffix = [0] * n
 
-        for i in range(1, len(nums), 1):
+        prefix[0] = nums[0]
+        for i in range(1, n, 1):
             prefix[i] = prefix[i - 1]  * nums[i]
 
-        for i in range(len(nums) - 2, -1, -1):
+        suffix[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
             suffix[i] = suffix[i + 1]  * nums[i]
         
-        output = []
+        output = [0] * n
 
-        output.append(suffix[1])
+        output[0] = suffix[1]
 
-        for i in range(0, len(nums) - 2):
-            output.append(prefix[i] * suffix[i + 2])
+        for i in range(0, n - 2):
+            output[i + 1] = prefix[i] * suffix[i + 2]
 
-        output.append(prefix[-2])
+        output[n - 1] = prefix[-2]
 
         return output
 
