003f4a99424ed1d6071e72cded060d5b063fc50c
Quang Do <quang.do@colorado.edu>
2025-01-16 19:46:05 -0700

    feat: add 1207

---
+++ 1207-unique-number-of-occurrences/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def uniqueOccurrences(self, arr: list[int]) -> bool:
+        count = {}
+        for n in arr:
+            if n in count:
+                count[n] += 1
+            else:
+                count[n] = 1
+
+        unique = {}
+        for k in count:
+            if count[k] in unique:
+                return False
+            else:
+                unique[count[k]] = 1
+
+        return True
+
+sol = Solution()
+print(sol.uniqueOccurrences([1,2,2,1,1,3]))
+print(sol.uniqueOccurrences([1,2]))
