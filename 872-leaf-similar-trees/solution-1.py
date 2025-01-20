0e5323c2b7fbd4e516a22e5bd589d61b71d73919
Quang Do <quang.do@colorado.edu>
2025-01-19 21:50:02 -0700

    feat: update 872

--- 872-leaf-similar-trees/solution-1.py
+++ 872-leaf-similar-trees/solution-1.py
@@ -32,19 +32,19 @@ class Solution:
             return []
 
         leaf_nodes = []
-        queue = [root]
+        stack = [root]
 
-        while len(queue) > 0:
-            node = queue.pop()
+        while len(stack) > 0:
+            node = stack.pop()
 
             if node.left is None and node.right is None:
                 leaf_nodes.append(node.val)
 
             if node.left is not None:
-                queue.append(node.left)
+                stack.append(node.left)
 
             if node.right is not None:
-                queue.append(node.right)
+                stack.append(node.right)
 
         return leaf_nodes
 
