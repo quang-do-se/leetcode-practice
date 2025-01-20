92295be2dcb94ef5fcfa58de9bb1d2104d74a73a
Quang Do <quang.do@colorado.edu>
2025-01-19 21:50:56 -0700

    feat: update 872

--- 872-leaf-similar-trees/solution-1.py
+++ 872-leaf-similar-trees/solution-1.py
@@ -40,11 +40,11 @@ class Solution:
             if node.left is None and node.right is None:
                 leaf_nodes.append(node.val)
 
-            if node.left is not None:
-                stack.append(node.left)
-
             if node.right is not None:
                 stack.append(node.right)
+
+            if node.left is not None:
+                stack.append(node.left)
 
         return leaf_nodes
 
