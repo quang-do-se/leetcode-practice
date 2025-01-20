4ccc203889832e23de964ebcfedea9258c36d200
Quang Do <quang.do@colorado.edu>
2025-01-19 19:55:08 -0700

    style: update

--- 1431-kids-with-the-greatest-number-of-candies/solution.py
+++ 1431-kids-with-the-greatest-number-of-candies/solution.py
@@ -1,5 +1,8 @@
+from typing import List
+
+
 class Solution:
-    def kidsWithCandies(self, candies: list[int], extraCandies: int) -> list[bool]:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         original_highest_candies = max(candies)
 
         new_list = []
@@ -9,7 +12,8 @@ class Solution:
 
         return new_list
 
+
 sol = Solution()
-print(sol.kidsWithCandies([2, 3, 5, 1, 3], 3))
-print(sol.kidsWithCandies([4, 2, 1, 1, 2], 1))
-print(sol.kidsWithCandies([12, 1, 12], 10))
+print(sol.kidsWithCandies([2, 3, 5, 1, 3], 3) == [True,True,True,False,True] )
+print(sol.kidsWithCandies([4, 2, 1, 1, 2], 1) == [True, False, False, False, False])
+print(sol.kidsWithCandies([12, 1, 12], 10) == [True, False, True])
