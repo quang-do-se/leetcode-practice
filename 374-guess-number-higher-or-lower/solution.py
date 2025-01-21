3c4ebf07350750ab32b9a8c7e14624c5fc7b5ad0
Quang Do <quang.do@colorado.edu>
2025-01-20 22:55:47 -0700

    feat: add 374

---
+++ 374-guess-number-higher-or-lower/solution.py
@@ -0,0 +1,51 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+pick = 0
+range = 0
+
+def guess(num: int) -> int:
+    if pick < num:
+        return -1
+    elif pick > num:
+        return 1
+
+    return 0
+
+class Solution:
+
+    def guessNumber(self, n: int) -> int:
+        low = 1
+        high = n
+
+        while True:
+            # mid = (low + high) // 2  # Could be overflow if low and high are big enough
+            mid = low + (high - low) // 2  # Avoid overflow == (2 * low + high - low) // 2
+            response = guess(mid)
+
+            if response > 0:  # guess is smaller than target
+                low = mid + 1
+            elif response < 0:  # guess is bigger than target
+                high = mid - 1
+            else:
+                return mid
+
+
+sol = Solution()
+
+
+range, pick = 10, 6
+print(sol.guessNumber(range) == 6)
+
+range, pick = 1, 1
+print(sol.guessNumber(range) == 1)
+
+range, pick = 2, 1
+print(sol.guessNumber(range) == 1)
+
+range, pick = 2, 2
+print(sol.guessNumber(range) == 2)
+
