404c3e0af05b041f87b96e849cf6975a7ff54a5d
Quang Do <quang.do@colorado.edu>
2025-01-22 00:37:18 -0700

    feat: update 338

--- 338-counting-bits/solution.py
+++ 338-counting-bits/solution.py
@@ -6,14 +6,15 @@ class Solution:
         res = []
         for i in range(n + 1):
             val = 0
-            while i > 0:
-                val += i & 1
-                i = i >> 1
+            while i != 0:
+                i = i & (i - 1)
+                val += 1
             res.append(val)
 
         return res
 
 sol = Solution()
+
 
 print(sol.countBits(2) == [0,1,1])
 print(sol.countBits(5) == [0,1,1,2,1,2])
