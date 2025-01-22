d1aea1ff39a13509d9d4b4b7135dd9d4e0b73922
Quang Do <quang.do@colorado.edu>
2025-01-21 23:50:09 -0700

    feat: add 191

---
+++ 191-number-of-1-bits/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        val = 0
+        while n > 0:
+            val += n & 1
+            n = n >> 1
+        return val
+    
+
+sol = Solution()
+print(sol.hammingWeight(11) == 3)
+print(sol.hammingWeight(128) == 1)
+print(sol.hammingWeight(2147483645) == 30)
