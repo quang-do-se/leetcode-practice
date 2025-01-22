fbe2ae35f060aef75be0adbf41db714d9c77df9e
Quang Do <quang.do@colorado.edu>
2025-01-21 18:28:16 -0700

    feat: add 338

---
+++ 338-counting-bits/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+        for i in range(n + 1):
+            val = 0
+            while i > 0:
+                val += i & 1
+                i = i >> 1
+            res.append(val)
+
+        return res
+
+sol = Solution()
+
+print(sol.countBits(2) == [0,1,1])
+print(sol.countBits(5) == [0,1,1,2,1,2])
