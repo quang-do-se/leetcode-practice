3db85974bdfef9248d0ee96be1cad247093c3b33
Quang Do <quang.do@colorado.edu>
2025-01-25 21:22:33 -0700

    feat: add 746

--- 746-min-cost-climbing-stairs/solution-1.py
+++ 746-min-cost-climbing-stairs/solution-1.py
@@ -2,16 +2,19 @@ from typing import List
 
 
 class Solution:
-    # Brute force approach
+    # Memoization approach
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        self.memoi = {}
         return min(self.min_cost_climbing_stairs(0, cost), self.min_cost_climbing_stairs(1, cost))
 
     def min_cost_climbing_stairs(self, index: int, cost: List[int]) -> int:
+        if index in self.memoi:
+            return self.memoi[index]
         if index >= len(cost) - 2:
             return cost[index]
         
-        total_cost = cost[index] + min(self.min_cost_climbing_stairs(index + 1, cost), self.min_cost_climbing_stairs(index + 2, cost))
-        return total_cost
+        self.memoi[index] = cost[index] + min(self.min_cost_climbing_stairs(index + 1, cost), self.min_cost_climbing_stairs(index + 2, cost))
+        return self.memoi[index]
 
 
 sol = Solution()
