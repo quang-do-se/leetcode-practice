47ac70983c31b5260556e641b468dd546278328c
Quang Do <quang.do@colorado.edu>
2025-01-27 10:04:33 -0700

    feat: add utility

--- 226-invert-binary-tree/solution.py
+++ 226-invert-binary-tree/solution.py
@@ -1,58 +1,11 @@
-from collections import deque
-from typing import List, Optional, Self
+import os
+import sys
+from typing import Optional
 
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "utility"))
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from tree_node import TreeNode
 
-    @staticmethod
-    def generate_from_array(binary_tree_array: List[int], index = 0) -> Self:
-        node = None
-
-        if index > len(binary_tree_array) - 1 or binary_tree_array[index] is None:
-            return node
-
-        if binary_tree_array[index] is not None:
-            node = TreeNode(val = binary_tree_array[index])
-
-        node.left = TreeNode.generate_from_array(binary_tree_array, index * 2 + 1)
-        node.right = TreeNode.generate_from_array(binary_tree_array, index * 2 + 2)
-
-        return node
-    
-    @staticmethod
-    def binary_tree_to_array(tree: Optional[Self]) -> List:
-        if tree is None:
-            return []
-
-        pre = []
-
-        queue = deque([(tree, 0)])
-        last_node = 0
-
-        while len(queue) > 0:
-            node, index = queue.popleft()
-            pre.append((node, index))
-
-            if node.left is not None:
-                left_index = index * 2 + 1
-                queue.append((node.left, left_index))
-                last_node = left_index
-            if node.right is not None:
-                right_index = index * 2 + 2
-                queue.append((node.right, right_index))
-                last_node = right_index
-
-        post = [None] * (last_node + 1)
-        for e in pre:
-            node, index  = e
-            post[index] = node.val
-
-        return post
-    
         
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
@@ -66,8 +19,8 @@ class Solution:
     
 
 sol = Solution()
-""" print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([4,2,7,1,3,6,9]))) == [4,7,2,9,6,3,1])
+print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([4,2,7,1,3,6,9]))) == [4,7,2,9,6,3,1])
 print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([2,1,3]))) == [2,3,1])
 print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([]))) == [])
-print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([1, 2]))) == [1, None, 2]) """
-print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([1, 2, None, 4]))))
+print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([1, 2]))) == [1, None, 2])
+print(TreeNode.binary_tree_to_array(sol.invertTree(TreeNode.generate_from_array([1, 2, None, 4]))) == [1,None,2,None,None,None,4])
