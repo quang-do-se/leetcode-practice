115b7480c08b73b03beef92fa26e64cb65dc61d4
Quang Do <quang.do@colorado.edu>
2025-02-04 19:14:23 -0700

    feat: update 1089

--- 1089-duplicate-zeros/solution.py
+++ 1089-duplicate-zeros/solution.py
@@ -5,30 +5,28 @@ from typing import List
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
         # Faster but more space
-        dup_count = 0
         hold = deque()
 
         i = 0
         while i < len(arr):
-            if dup_count > 0:
-                hold.append(arr[i])
-                arr[i] = 0
-                dup_count -= 1
-                i += 1
-                continue
-
             if hold:
                 hold.append(arr[i])
                 arr[i] = hold.popleft()
 
             if arr[i] == 0:
-                dup_count += 1
+                if i + 1 < len(arr):
+                    hold.append(arr[i + 1])
+                    arr[i + 1] = 0
+                i += 1
+                
             i += 1
+
 
 
 sol = Solution()
 arr = [1,0,2,3,0,4,5,0]
 sol.duplicateZeros(arr)
+print(arr)
 print(arr == [1, 0, 0, 2, 3, 0, 0, 4])
 
 arr = [0,2,0,0,5,0]
@@ -50,3 +48,7 @@ print(arr == [1, 2, 3])
 arr = [0]
 sol.duplicateZeros(arr)
 print(arr == [0])
+
+arr = [1,5,2,0,6,8,0,6,0]
+sol.duplicateZeros(arr)
+print(arr == [1, 5, 2, 0, 0, 6, 8, 0, 0])
