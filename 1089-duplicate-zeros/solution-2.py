29cb6b3ae96557610b3df5534bc99d32bdb7c4d7
Quang Do <quang.do@colorado.edu>
2025-02-06 05:22:33 -0700

    feat: update 1089

--- 1089-duplicate-zeros/solution-2.py
+++ 1089-duplicate-zeros/solution-2.py
@@ -3,12 +3,16 @@ from typing import List
 
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
+        # Most elegant solution: https://leetcode.com/problems/duplicate-zeros/solutions/322576/python-3-real-in-place-solution/
+
         zeroes = arr.count(0)
         n = len(arr)
         for i in range(n-1, -1, -1):
+            # Write original number in the new position, if the position can fit in the original array
             if i + zeroes < n:
                 arr[i + zeroes] = arr[i]
-                
+
+            # Write the duplicate zero
             if arr[i] == 0: 
                 zeroes -= 1
                 if i + zeroes < n:
