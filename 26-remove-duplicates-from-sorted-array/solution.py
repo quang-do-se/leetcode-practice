b8bf1e8eb6c24cd07a643ca01dfb0a3f14542c35
Quang Do <quang.do@colorado.edu>
2025-02-07 18:46:06 -0700

    feat: update 26

--- 26-remove-duplicates-from-sorted-array/solution.py
+++ 26-remove-duplicates-from-sorted-array/solution.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        w = r = 0
+        w = 0
         prev = None
         for r in range(len(nums)):
             if nums[r] != prev:
@@ -14,14 +14,17 @@ class Solution:
 
 sol = Solution()
 nums = [0,0,1,1,1,2,2,3,3,4]
-print(sol.removeDuplicates(nums))
+print(sol.removeDuplicates(nums) == 5)
 print(nums)
 
-
 nums = [1,1,2]
-print(sol.removeDuplicates(nums))
+print(sol.removeDuplicates(nums) == 2)
 print(nums)
 
 nums = [1]
-print(sol.removeDuplicates(nums))
+print(sol.removeDuplicates(nums) == 1)
+print(nums)
+
+nums = []
+print(sol.removeDuplicates(nums) == 0)
 print(nums)
