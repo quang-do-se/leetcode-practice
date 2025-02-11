dcef6e45a697bdf23fc0ee6fd9a22453bd287bac
Quang Do <quang.do@colorado.edu>
2025-02-11 07:58:04 -0700

    chore: update 1051

--- 1051-height-checker/solution.py
+++ 1051-height-checker/solution.py
@@ -4,16 +4,17 @@ from typing import List
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         freq = [0] * 101
-        for e in heights:
-            freq[e] = freq[e] + 1
+        for val in heights:
+            freq[val] = freq[val] + 1
 
+        # prefix sum of frequencies
         for i in range(2, len(freq)):
             freq[i] += freq[i - 1]
 
         not_in_order_count = 0
         for i in range(0, len(heights)):
-            e = heights[i]
-            if i < freq[e - 1] or i >= freq[e]:
+            val = heights[i]
+            if i < freq[val - 1] or i >= freq[val]:
                 not_in_order_count += 1
 
         return not_in_order_count
