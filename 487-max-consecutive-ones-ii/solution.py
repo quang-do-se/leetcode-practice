ffb3caebb769b6ffb71cf8d45c77798ceb71cc2c
Quang Do <quang.do@colorado.edu>
2025-02-11 17:29:07 -0700

    feat: add 487

---
+++ 487-max-consecutive-ones-ii/solution.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        prev_one_count = 0
+        curr_one_count = 0
+        zero_count = 0
+        max_one_count = 0
+
+        for e in nums:
+            if e == 1:
+                curr_one_count += 1
+            else:
+                if curr_one_count > 0:
+                    extend_one_count = curr_one_count + 1
+                    if zero_count == 1:
+                        extend_one_count = curr_one_count + prev_one_count + 1
+
+                    max_one_count = max(extend_one_count, max_one_count)
+
+                    prev_one_count = curr_one_count
+                    curr_one_count = 0
+                    zero_count = 1
+                else:
+                    zero_count += 1
+
+        
+        if zero_count == 0:
+            return curr_one_count
+        elif zero_count == 1:
+            max_one_count = max(curr_one_count + prev_one_count + 1, max_one_count)
+        else:
+            max_one_count = max(curr_one_count + 1, max_one_count)
+
+        return max_one_count
+
+
+sol = Solution()
+print(sol.findMaxConsecutiveOnes([0, 1, 1, 1]) == 4)
+print(sol.findMaxConsecutiveOnes([1, 1, 1, 1]) == 4)
+print(sol.findMaxConsecutiveOnes([1, 1, 1, 0]) == 4)
+print(sol.findMaxConsecutiveOnes([1, 1, 1, 0, 1, 1]) == 6)
+print(sol.findMaxConsecutiveOnes([1, 1, 1, 0, 0, 1, 1]) == 4)
+print(sol.findMaxConsecutiveOnes([1, 1, 0, 0, 1, 1, 1, 1]) == 5)
+print(sol.findMaxConsecutiveOnes([0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0]) == 7)
+print(sol.findMaxConsecutiveOnes([0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0]) == 7)
