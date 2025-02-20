c19bf40b6de90213a76412795cf865789e6a78de
Quang Do <quang.do@colorado.edu>
2025-02-20 02:07:58 -0700

    chore: update 21

---
+++ 21-merge-two-sorted-lists/solution-1.py
@@ -0,0 +1,43 @@
+from typing import Optional
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "utility"))
+
+from list_node import ListNode, LinkedList
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+
+        sorted_list = ListNode(0)
+        tail = sorted_list
+
+        while list1 or list2:
+            if not list2 or (list1 and list1.val < list2.val):
+                tail.next = list1
+                tail = tail.next
+                list1 = list1.next
+            elif not list1 or (list2 and list1.val >= list2.val):
+                tail.next = list2
+                tail = tail.next
+                list2 = list2.next
+            
+            tail.next = None
+
+        return sorted_list.next
+
+
+
+
+sol = Solution()
+print(LinkedList.list_to_array(sol.mergeTwoLists(LinkedList.generate_from_array([1,2,4]), LinkedList.generate_from_array([1,3,4]))) == [1, 1, 2, 3, 4, 4])
+print(LinkedList.list_to_array(sol.mergeTwoLists(LinkedList.generate_from_array([2]), LinkedList.generate_from_array([1]))) == [1, 2])
+print(LinkedList.list_to_array(sol.mergeTwoLists(LinkedList.generate_from_array([1]), LinkedList.generate_from_array([2]))) == [1, 2])
+print(LinkedList.list_to_array(sol.mergeTwoLists(LinkedList.generate_from_array([1]), LinkedList.generate_from_array([]))) == [1])
+print(LinkedList.list_to_array(sol.mergeTwoLists(LinkedList.generate_from_array([]), LinkedList.generate_from_array([2]))) == [2])
+print(LinkedList.list_to_array(sol.mergeTwoLists(LinkedList.generate_from_array([5]), LinkedList.generate_from_array([1,2,4]))) == [1, 2, 4, 5])
