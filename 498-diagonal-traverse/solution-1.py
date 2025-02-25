01ff7be9068462d510e2de8cea793602d97cd655
Quang Do <quang.do@colorado.edu>
2025-02-25 05:35:37 -0700

    chore: update 498

---
+++ 498-diagonal-traverse/solution-1.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        up_right_direction = (-1, 1)
+        down_left_direction = (1,- 1)
+
+        height = len(mat)
+        width = len(mat[0])
+
+        res = []
+
+        direction = up_right_direction
+        row, col = 0, 0
+
+        while row < height and col < width:
+            res.append(mat[row][col])
+            
+            next_row = row + direction[0]
+            next_col = col + direction[1]
+            
+            switch_direction = False
+
+            if next_row < 0 or next_row > height - 1:
+                next_row = row
+                next_col = col + 1
+                switch_direction = True
+
+            if next_col < 0 or next_col > width - 1:
+                next_row = row + 1
+                next_col = col
+                switch_direction = True
+            
+            if switch_direction:
+                if direction == up_right_direction:
+                    direction = down_left_direction
+                else:
+                    direction = up_right_direction
+
+            row, col = next_row, next_col
+
+        return res
+
+
+sol = Solution()
+print(sol.findDiagonalOrder([[1,2,3,4,5,6], [7,8,9,10,11,12], [13,14,15,16,17,18], [19,20,21,22,23,24], [25,26,27,28,29,30], [31,32,33,34,35,36]]))
+print(sol.findDiagonalOrder([[1,2,3], [4,5,6], [7,8,9]]))
+print(sol.findDiagonalOrder([[1,2], [3,4]]))
