2947924afc2975214e532e326acc62db53d81685
Quang Do <quang.do@colorado.edu>
2025-03-08 23:02:48 -0700

    feat: add 1

---
+++ 1-two-sum/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = dict()
+        for i in range(len(nums)):
+            k = target - nums[i]
+
+            if k in seen:
+                return [seen[k], i]
+
+            seen[nums[i]] = i
+
+        return []
+    
+
+sol = Solution()
+print(sol.twoSum([2,7,11,15], 9))
+print(sol.twoSum([3,2,4], 6))
+print(sol.twoSum([3,3], 6))
+print(sol.twoSum([3,1,0,3], 6))
