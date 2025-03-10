925ef76039f952479a425f7ca5ae6e0ec3a125e4
Quang Do <quang.do@colorado.edu>
2025-03-10 00:42:15 -0600

    fix: 205

--- 205-isomorphic-strings/solution.py
+++ 205-isomorphic-strings/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         map = {}
-        seen = {}
+        seen = set()
         for i in range(len(s)):
             if s[i] in map:
                 if t[i] != map[s[i]]:
                     return False
             else:
+                if t[i] in seen:
+                    return False
+                else:
+                    seen.add(t[i])
+
                 map[s[i]] = t[i]
+
 
         return True
 
@@ -16,3 +22,4 @@ sol = Solution()
 print(sol.isIsomorphic("egg", "add"))
 print(sol.isIsomorphic("foo", "bar"))
 print(sol.isIsomorphic("paper", "title"))
+print(sol.isIsomorphic("badc", "baba"))
