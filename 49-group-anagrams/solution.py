631e9be30c35d56ba71ab8290a3123332cdf693f
Quang Do <quang.do@colorado.edu>
2025-07-22 23:09:28 -0600

    Update

---
+++ 49-group-anagrams/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        map = {}
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            if sorted_s in map:
+                map[sorted_s].append(s)
+            else:
+                map[sorted_s] = [s]
+
+        res = list()
+        for e in map:
+            res.append(map[e])
+
+        return res
+    
+
+sol = Solution()
+print(sol.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']])
+print(sol.groupAnagrams(["", ""]) == [["", ""]])
