e3343e7750b33c1a904cb755578eae1f4f92d7a0
Quang Do <quang.do@colorado.edu>
2025-07-27 14:58:11 -0600

    feat: update 652

--- 652-find-duplicate-subtrees/solution-2.py
+++ 652-find-duplicate-subtrees/solution-2.py
@@ -14,6 +14,8 @@ class Solution:
         self.count = {}
         self.result = []
         self.serialize_node(root)
+        
+        #print(self.triplet_to_id)
 
         return self.result
 
@@ -41,10 +43,11 @@ class Solution:
         return id
 
 
-# [0,2,3,4,3,2,4,null,null,null,null,4,3]
-# [7,6,8,7,7,7,7,6,8,8,8,6,8,6,6,5,5,7,9,7,9,7,null,7,7,9,9,null,null,7,7,4,6,null,6,6,null,8,null,6,8,null,null,null,null,6,8,8,6,null,8,null,null,6,8,8,8,3,5,7,7,null,null,5,7,7,null,null,null,null,null,7,null,null,null,null,null,5,5,null,null,null,null,null,null,null,9,null,9,null,4,null,null,8,6,6,null,4,null,6,8,6,null,null,null,null,6,4,6,8,8,null,null,null,null,7,7,5,7,null,null,null,null,null,null,7,null,5,null,5,null,null,5,5,null,null,null,7,null,6,6,null,8,4,null,6,null,null,null,null,6,null,null,6,6,null,null,null,null,null,7,5,null,null,null,null,null,null,null,5,7,5,5,null,null,8,null,6,null,null,null,8,6,6,6,null,null,null,null,5]
 sol = Solution()
 
-# print(sol.findDuplicateSubtrees(BinaryTree.from_array([0,1,2,None,3,4,5])))
-# print(sol.findDuplicateSubtrees(BinaryTree.from_array([2,2,2,None,3,None,3])))
-print(sol.findDuplicateSubtrees(BinaryTree.from_array([7,6,8,7,7,7,7,6,8,8,8,6,8,6,6,5,5,7,9,7,9,7,None,7,7,9,9,None,None,7,7,4,6,None,6,6,None,8,None,6,8,None,None,None,None,6,8,8,6,None,8,None,None,6,8,8,8,3,5,7,7,None,None,5,7,7,None,None,None,None,None,7,None,None,None,None,None,5,5,None,None,None,None,None,None,None,9,None,9,None,4,None,None,8,6,6,None,4,None,6,8,6,None,None,None,None,6,4,6,8,8,None,None,None,None,7,7,5,7,None,None,None,None,None,None,7,None,5,None,5,None,None,5,5,None,None,None,7,None,6,6,None,8,4,None,6,None,None,None,None,6,None,None,6,6,None,None,None,None,None,7,5,None,None,None,None,None,None,None,5,7,5,5,None,None,8,None,6,None,None,None,8,6,6,6,None,None,None,None,5])))
+# sol.findDuplicateSubtrees(BinaryTree.from_array([0,1,2,None,3,4,5]))
+# sol.findDuplicateSubtrees(BinaryTree.from_array([2,2,2,None,3,None,3]))
+
+result = sol.findDuplicateSubtrees(BinaryTree.from_array([7,6,8,7,7,7,7,6,8,8,8,6,8,6,6,5,5,7,9,7,9,7,None,7,7,9,9,None,None,7,7,4,6,None,6,6,None,8,None,6,8,None,None,None,None,6,8,8,6,None,8,None,None,6,8,8,8,3,5,7,7,None,None,5,7,7,None,None,None,None,None,7,None,None,None,None,None,5,5,None,None,None,None,None,None,None,9,None,9,None,4,None,None,8,6,6,None,4,None,6,8,6,None,None,None,None,6,4,6,8,8,None,None,None,None,7,7,5,7,None,None,None,None,None,None,7,None,5,None,5,None,None,5,5,None,None,None,7,None,6,6,None,8,4,None,6,None,None,None,None,6,None,None,6,6,None,None,None,None,None,7,5,None,None,None,None,None,None,None,5,7,5,5,None,None,8,None,6,None,None,None,8,6,6,6,None,None,None,None,5]))
+for node in result:
+    BinaryTree.visualize(node)
