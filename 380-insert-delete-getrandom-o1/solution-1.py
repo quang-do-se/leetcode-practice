a9f5ffc0158111ca44fe162803c36d759d6d4398
Quang Do <quang.do@colorado.edu>
2025-08-08 18:24:12 -0600

    feat: 380

---
+++ 380-insert-delete-getrandom-o1/solution-1.py
@@ -0,0 +1,55 @@
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.num_to_index = {}
+        self.list = []
+        self.count = 0
+
+    def insert(self, val: int) -> bool:
+        if val in self.num_to_index:
+            return False
+        
+        self.num_to_index[val] = len(self.list)
+        self.list.append(val)
+
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.num_to_index:
+            val_index = self.num_to_index[val]
+
+            if val_index == len(self.list) - 1:
+                self.list.pop()
+            else:
+                last_element = self.list.pop()
+                self.list[val_index] = last_element
+                self.num_to_index[last_element] = val_index
+
+            self.num_to_index.pop(val)
+
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        return random.choice(self.list)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+obj = RandomizedSet()
+obj.insert(1)
+obj.insert(1)
+obj.insert(2)
+obj.insert(3)
+obj.insert(4)
+
+print("Random:", obj.getRandom())
+print("Random:", obj.getRandom())
+print("Random:", obj.getRandom())
+print("Random:", obj.getRandom())
+print("Random:", obj.getRandom())
+print("Random:", obj.getRandom())
+print("Random:", obj.getRandom())
+print("Random:", obj.getRandom())
