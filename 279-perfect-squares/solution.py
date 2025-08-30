8111447cfd9881bcf4cc62d713e3553f379bee37
Quang Do <quang.do@colorado.edu>
2025-08-29 21:29:48 -0600

    feat: add 279

---
+++ 279-perfect-squares/solution.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        perfect_square_numbers = set()
+
+        i = 1
+        square_number = i * i
+        while square_number <= n:
+            perfect_square_numbers.add(square_number)            
+            i += 1
+            square_number = i * i
+
+        queue = deque([(0, 0)])
+        visited = set([(0, 0)])
+
+        while queue:
+            size_at_current_level = len(queue)
+
+            for _ in range(size_at_current_level):
+                
+                sum, steps = queue.popleft()
+
+                if sum == n:
+                    return steps
+                
+                for square_number in perfect_square_numbers:
+                    new_sum = (sum + square_number, steps + 1)
+                    if new_sum not in visited:
+                        queue.append(new_sum)
+                        visited.add(new_sum)
+            
+        return -1
+
+
+sol = Solution()
+print(sol.numSquares(1) == 1)
+print(sol.numSquares(2) == 2)
+print(sol.numSquares(3) == 3)
+print(sol.numSquares(36) == 1)
+print(sol.numSquares(12) == 3)
+print(sol.numSquares(13) == 2)
+print(sol.numSquares(9999) == 4)
+print(sol.numSquares(10 ** 4) == 1)
+
