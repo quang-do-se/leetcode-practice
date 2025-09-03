272444ba95483512762227f10074595d2a0bcf4c
Quang Do <quang.do@colorado.edu>
2025-09-03 00:16:45 -0600

    feat: add 739

--- 739-daily-temperatures/solution.py
+++ 739-daily-temperatures/solution.py
@@ -9,16 +9,17 @@ class Solution:
             while stack and temperatures[i] > stack[-1][0]:
                 res[stack[-1][1]] = i - stack[-1][1]
                 stack.pop()
-              
+
             stack.append((temperatures[i], i))
 
         return res
-            
 
 
 sol = Solution()
-print(sol.dailyTemperatures([73,74,75,71,69,72,76,73]) == [1, 1, 4, 2, 1, 1, 0, 0])
-print(sol.dailyTemperatures([30,40,50,60]) == [1,1,1,0])
-print(sol.dailyTemperatures([30,60,90]) == [1,1,0])
-print(sol.dailyTemperatures([60,60,60]) == [0,0,0])
-print(sol.dailyTemperatures([60,50,40]) == [0,0,0])
+print(
+    sol.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]) == [1, 1, 4, 2, 1, 1, 0, 0]
+)
+print(sol.dailyTemperatures([30, 40, 50, 60]) == [1, 1, 1, 0])
+print(sol.dailyTemperatures([30, 60, 90]) == [1, 1, 0])
+print(sol.dailyTemperatures([60, 60, 60]) == [0, 0, 0])
+print(sol.dailyTemperatures([60, 50, 40]) == [0, 0, 0])
