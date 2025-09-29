4e9aac2cc2cea626378a72c68bfe33a288e11300
Quang Do <quang.do@colorado.edu>
2025-09-29 11:19:24 -0600

    chore: update 3571

---
+++ 3571-find-the-shortest-superstring-ii/solution-optimized.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestSuperstring(self, s1: str, s2: str) -> str:
+        n, m = len(s1), len(s2)
+
+        if n > m:
+            s1, s2, n = s2, s1, m
+
+        if s1 in s2: return s2
+
+        for i in range(1, n):
+            if s1[i:] == s2[:n-i]:
+                return s1[:i] + s2
+            if s2[-n+i:] == s1[:-i]:
+                return s2 + s1[n-i:]
+
+        return s1 + s2
+    
+
+sol = Solution()
+
+
+print(sol.shortestSuperstring("gwb", "slfg") == "slfgwb")
+print(sol.shortestSuperstring("kbcdefg", "defghijk") == "kbcdefghijk")
