d0e3b31978a16a9bea3a882e72c6f583bfd0b374
Quang Do <quang.do@colorado.edu>
2025-10-02 13:22:52 -0600

    chore: update 2873

--- 2873-maximum-value-of-an-ordered-triplet-i/solution.py
+++ 2873-maximum-value-of-an-ordered-triplet-i/solution.py
@@ -5,25 +5,27 @@ class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         n = len(nums)
 
-        max = 0
+        max_total = 0
         for i in range(n):
+            max_sub = 0
             for j in range(i + 1, n):
                 if nums[i] <= nums[j]:
                     continue
                 else:
                     sub = nums[i] - nums[j]
+                    if sub > max_sub:
+                        for k in range (j + 1, n):
+                            total = sub * nums[k]
 
-                    for k in range (j + 1, n):
-                        total = sub * nums[k]
+                            if total > max_total:
+                                max_total = total
+                        max_sub = sub
 
-                        if total > max:
-                            max = total
-
-        return max
+        return max_total
 
 
 
 sol = Solution()
-print(sol.maximumTripletValue([12,6,1,2,7]))
-print(sol.maximumTripletValue([1,10,3,4,19]))
-print(sol.maximumTripletValue([1,2,3]))
+print(sol.maximumTripletValue([12,6,1,2,7]) == 77)
+print(sol.maximumTripletValue([1,10,3,4,19]) == 133)
+print(sol.maximumTripletValue([1,2,3]) == 0)
